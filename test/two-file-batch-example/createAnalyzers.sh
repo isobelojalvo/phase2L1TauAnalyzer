16a4108605155ea561da38b4310bcb4c6edd8633
isobelojalvo <ojalvo@wisc.edu>
2019-07-23 17:41:44 -0500

    changing the structure of input and output file structure

--- test/two-file-batch-example/createAnalyzers.sh
+++ test/two-file-batch-example/createAnalyzers.sh
@@ -1,7 +1,7 @@
-jobName="2019_Apr11-dyll"
+jobName="2019_Jul27-dyll-test-2"
 #
 j=0
-for i in {0..79}
+for i in {0..1}
 do
     cat test-Analyzer.py > SUB-Analyzer-${i}.py
     #echo "process.source.skipEvents = cms.untracked.uint32(${j})" >> SUB-Analyzer-${i}.py
@@ -9,9 +9,9 @@ do
     cat submit-$i.py >> SUB-Analyzer-${i}.py
     cat submit.py >> SUB-Analyzer-${i}.py
 
-    mkdir -p /nfs_scratch/ojalvo/${jobName}-DYLL-200PU-SUBPhase-$i/dags/daginputs
-    
-    farmoutAnalysisJobs --vsize-limit=7000 --site-requirements='OpSysAndVer == "SL6"' --assume-input-files-exist  --input-file-list=inputFileList-mini.txt --submit-dir=/nfs_scratch/ojalvo/${jobName}-DYLL-200PU-SUBPhase-$i/submit --output-dag-file=/nfs_scratch/ojalvo/${jobName}-DYLL-200PU-SUBPhase-$i/dags/dag  ${jobName}-DYLL-200PU  $CMSSW_BASE  $CMSSW_BASE/src/L1Trigger/L1MTD/test/run_in_parallel-200PU-DYll/SUB-Analyzer-$i.py     &
+    mkdir -p /nfs_scratch/ojalvo/${jobName}/SUB-DYLL-200PU-SUBPhase-$i/dags/daginputs
+
+    farmoutAnalysisJobs --vsize-limit=7000 --assume-input-files-exist  --input-file-list=inputFileList-mini.txt --output-dir=/hdfs/store/user/ojalvo/${jobName} --submit-dir=/nfs_scratch/ojalvo/${jobName}/SUB-DYLL-200PU-SUBPhase-$i/submit --output-dag-file=/nfs_scratch/ojalvo/${jobName}/SUB-DYLL-200PU-SUBPhase-$i/dags/dag  ${jobName}-DYLL-200PU  $CMSSW_BASE  $CMSSW_BASE/src/L1Trigger/phase2L1TauAnalyzer/test/two-file-batch-example/SUB-Analyzer-$i.py     &
     
     if [ "$i" -eq "15" ]; then
 	wait;
